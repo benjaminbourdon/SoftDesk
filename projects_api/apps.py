aaea9ddd484090cd281153d0df71371a6fbcd01d
Benjamin B <dev@benjamin-b.fr>
2023-05-30 01:58:36 +0200

    first commit

---
+++ projects_api/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ProjectsApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "projects_api"
+    verbose_name = "projects management APi"
