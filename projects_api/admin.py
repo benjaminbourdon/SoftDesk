25fbb7f84e0f1dd2fac817a8c5d2d082d762213e
Benjamin B <dev@benjamin-b.fr>
2023-06-01 17:19:04 +0200

    Improve Project and Contributor admin interface

--- projects_api/admin.py
+++ projects_api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from projects_api.models import Project, Issue, Contributor, Comment
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "type")
+    list_filter = ["type"]
+    ordering = ["-id"]
+
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ("project", "user")
+    list_filter = ("project", "user")
+    ordering = ["-id"]
+
+
+admin.site.register(Issue)
+admin.site.register(Comment)
