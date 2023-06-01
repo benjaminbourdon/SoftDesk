5fefe03b7c15002be8e3a08f56b6cd466b135738
Benjamin B <dev@benjamin-b.fr>
2023-06-01 17:18:41 +0200

    Modify models name and project attributes

--- projects_api/models.py
+++ projects_api/models.py
@@ -2,34 +2,62 @@ from django.conf import settings
 from django.db import models
 
 
-class Projects(models.Model):
+class Project(models.Model):
     class Plateforms(models.TextChoices):
         BACKEND = "BE", "Back-End"
         FRONTEND = "FE", "Front-End"
         IOS = "IO", "iOS"
         ANDROID = "AN", "Android"
 
-    title = models.CharField(max_length=128)
-    description = models.CharField(max_length=2048)
-    type = models.CharField(max_length=2, choices=Plateforms.choices)
+    title = models.CharField(max_length=128, verbose_name="titre")
+    description = models.CharField(max_length=2048, verbose_name="description")
+    type = models.CharField(
+        max_length=2,
+        choices=Plateforms.choices,
+        verbose_name="Type (plateforme)",
+    )
     # author = models.ForeignKey(
     #     to=settings.AUTH_USER_MODEL,
     #     on_delete=models.PROTECT,
+    #     verbose_name = "Auteurice"
     # )
 
+    def __str__(self):
+        return f"#{self.id} - {self.title}"
 
-class Contributors(models.Model):
+    class Meta:
+        verbose_name = "Projet"
+
+
+class Contributor(models.Model):
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="contributions",
     )
-    project = models.ForeignKey(to=Projects, on_delete=models.CASCADE)
+    project = models.ForeignKey(
+        to=Project,
+        on_delete=models.CASCADE,
+        related_name="contributors",
+    )
     # permission = models.TextChoices()
     # role = models.CharField()
 
+    def __str__(self):
+        return f"{self.user} participe à {self.project.title}"
 
-class Issues(models.Model):
+    @property
+    def contributor_username(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = (
+            "user",
+            "project",
+        )
+
+
+class Issue(models.Model):
     class Tag(models.TextChoices):
         BUG = "BUG", "Bug"
         IMPROVEMENT = "IMPROVE", "Amélioration"
@@ -53,7 +81,7 @@ class Issues(models.Model):
         choices=Priority.choices,
         default=Priority.MODERATE,
     )
-    project = models.ForeignKey(to=Projects, on_delete=models.CASCADE)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=10,
         choices=Status.choices,
@@ -73,7 +101,7 @@ class Issues(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     description = models.CharField(max_length=2048)
     author = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
@@ -81,7 +109,7 @@ class Comments(models.Model):
         related_name="created_comments",
     )
     issue = models.ForeignKey(
-        to=Issues,
+        to=Issue,
         on_delete=models.CASCADE,
     )
     created_time = models.DateTimeField(auto_now_add=True)
