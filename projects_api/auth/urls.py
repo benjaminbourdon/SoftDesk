35b9e5bd0344d50d3964b2dec10ff5d1c5428b8c
Benjamin B <dev@benjamin-b.fr>
2023-06-19 18:33:52 +0200

    Fix bug of multiple identic urls creating csrf bug

--- projects_api/auth/urls.py
+++ projects_api/auth/urls.py
@@ -4,8 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView
 
 urlpatterns = [
-    path("", include("rest_framework.urls")),
     path("signup/", RegisterView.as_view(), name="signup"),
-    path("api/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("html/", include("rest_framework.urls")),
 ]
