c24d06013a33c506cf5322562ac4a86c662f8227
Benjamin B <dev@benjamin-b.fr>
2023-06-19 18:35:49 +0200

    Add assignee validation for issues

--- projects_api/serializers/issuesserializer.py
+++ projects_api/serializers/issuesserializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from projects_api.models import Issue, Project
+from projects_api.models import Issue, Project, Contributor, choicesclass
 
 
 class IssuesSerializer(serializers.ModelSerializer):
@@ -29,3 +29,17 @@ class IssuesSerializer(serializers.ModelSerializer):
         validated_data["author"] = self.context["request"].user
         instance = super().create(validated_data)
         return instance
+
+    def validate_assignee(self, value):
+        """Check that assigned user is a project contributor."""
+        queryset = Contributor.objects.filter(
+            project__id=self.context["view"].kwargs["project_pk"],
+            user__id=value.id,
+            permission__gte=choicesclass.Permission.CONTRIBUTOR,
+        )
+        if queryset.exists():
+            return value
+        else:
+            raise serializers.ValidationError(
+                "Assigned user must be a contributor of this project."
+            )
